bec968566ace020e74d384fd3f117f741f832a34
hill1588 <hill1588@morris.umn.edu>
2019-10-10 11:26:19 -0500

    Update to Echo*.java files
    started the pseudocode for both of the files and did some reading and writing on client end.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,45 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static final int portNumber = 6013;
+
+	public static void main(String[] args) throws IOException{
+		String server;
+
+		if (args.length == 0)
+			server = "127.0.0.1";
+		else 
+			server = args[0];
+
+		try {
+      			// Connect to the server
+      			Socket socket = new Socket(server, portNumber);
+
+      			// Get the input stream so we can read from that socket
+      			InputStream input = socket.getInputStream();
+     		//	BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			int read = inputStream.read();
+      			// Print all the input we receive from the server
+      		//	String line;
+     			while (read  != -1) {
+			System.out.println(socket.write(read));
+		
+
+      			}
+
+     			 // Close the socket when we're done reading from it
+      			socket.close();
+
+  			  // Provide some minimal error handling.
+    		} catch (ConnectException ce) {
+			System.out.println("We were unable to connect to " + server);
+     			System.out.println("You should make sure the server is running.");
+    		} catch (IOException ioe) {
+		       	System.out.println("We caught an unexpected exception");
+      			System.err.println(ioe);
+
+		}
+	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,14 @@
 package echoserver;
 
 public class EchoServer {
+	public static final int portNumber = 6013;
+
+	public static void main(String[] args) {
+		try {
+			ServerSocket sock = new ServerSocket(portNumber);
+			
+			while(true) {
+				Socket client = socekt.accept();
+				System.out.println("Got a request!");
+
 }
