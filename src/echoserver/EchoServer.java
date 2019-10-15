7ac7baea0ed70713ad766550a4de9848bc8990ff
hill1588 <hill1588@morris.umn.edu>
2019-10-15 10:54:20 -0500

    update to EchoServer.java and fix to issue where it was not passing small and large test files
    issue was with the while loop not correctly reading the correct output, when changing the int read to not = anything then it passed.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,14 +1,38 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
 	public static final int portNumber = 6013;
 
 	public static void main(String[] args) {
+
 		try {
 			ServerSocket sock = new ServerSocket(portNumber);
 			
+			 
+                        
 			while(true) {
-				Socket client = socekt.accept();
+				Socket client = sock.accept();
 				System.out.println("Got a request!");
+			        InputStream  input = client.getInputStream();
+				int read;
+                                OutputStream output = client.getOutputStream();
+				while((read = input.read()) != -1){
+                                     output.write(read);
+				}
 
+		
+				client.close();
+				input.close();
+				output.close();
+
+			}
+
+		    } catch (IOException ioe) {
+                      System.out.println("We caught an unexpected exception");
+                      System.err.println(ioe);
+                    }
+                               
+	} 
 }
